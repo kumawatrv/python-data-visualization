4f8713ca3c53607489e79f1b3d125df9e8bbb392
kumawatrv <72191708+kumawatrv@users.noreply.github.com>
2021-10-06 23:45:43 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#initializing x,y lists
+x=[1,2,3,4,5]
+y=[0,2,4,6,8]
+
+#resizing the graph
+plt.figure(figsize=(5,4),dpi=100)
+
+#plotting the graph
+plt.plot(x,y)
