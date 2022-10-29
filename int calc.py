c451fc61c20c07ca923507a2a74b5c27561b9e9b
Ravi Kumawat <49914487+ravikumawat7716@users.noreply.github.com>
2022-10-29 12:06:00 +0530

    Update int calc.py

--- int calc.py
+++ int calc.py
@@ -5,5 +5,5 @@ b=input("Enter the intrest rate percent per year ");
 c=input("Enter the time in years ");
 d=float(a)*float(b)*float(c)/100;
 print("The Intrest is ",d);
-print("The total Amount to be paid/taken is",float(a)+d);
+print("The total Amount to be paid/taken is",float(a)+d); // Total Amount
 
